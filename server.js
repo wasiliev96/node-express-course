5907811a966049174c475f4021dda73c8e3d4a8e
wasiliev96 <wasiliev.cloud@gmail.com>
2021-01-17 09:39:15 +0200

    add first GET route

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.listen(8000, ()=>{
-    console.log(`server is running`);
-})
+app.listen(8000, () => {
+  console.log(`server is running`);
+});
+
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+app.get("/users", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData,
+  });
+});
